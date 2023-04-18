038930438b2ee692b145bb1dc38dde33c8f4e018
Emily Rowley <erowley49@gmail.com>
2023-04-18 15:04:48 -0600

    add routes folder

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const app = express();
+var app = express();
+
+app.use('/', require('./routes'));
 
 const port = 8081;
-
-app.get('/test', function (req, res) {
-    res.send('Hello World');
-});
 
 var server = app.listen(port, function () {
     console.log(`Listening on ${port} for something cool`);
